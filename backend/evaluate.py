dd761eed566e205a0870344828903147a00e281c
George Botev <12297345+gbotev1@users.noreply.github.com>
2021-05-13 17:19:29 -0400

    Added our method iteration step

--- backend/evaluate.py
+++ backend/evaluate.py
@@ -2,7 +2,11 @@ import pickle
 from collections import defaultdict
 
 from GoogleNews import GoogleNews
+from sqlitedict import SqliteDict
 from tqdm import tqdm
+
+from api import get_nearest, doc_vecs_db_path
+from utils import string2vec
 
 NUM_QUERIES = 100
 NUM_PAGES = 10
@@ -23,15 +27,21 @@ def main() -> None:
     # Set up Google News fetcher for plausible date range of start of 2020 to date after we scraped
     # (to avoid potential boundary condition)
     gn = GoogleNews(lang='en', start='01/01/2020', end='05/07/2021')
-    # Perform evaluation
-    query2urls = {}
+    # Poll Google News for evaluation queries and save results
+    query2urls_relevant = {}
     for query, _ in tqdm(queries2count, total=NUM_QUERIES, desc='Polling GoogleNews Step'):
         gn.search(query)
         urls = []
         for i in range(1, NUM_PAGES + 1):  # Make sue NUM_PAGES is included
             articles = gn.page_at(i)  # GoogleNews is one-based
             urls.extend([article['link'] for article in articles])
-        query2urls[query] = urls
+        query2urls_relevant[query] = urls
+    # Obtain ordering of documents from our system for the same evaluation queries
+    query2url_results = {}
+    doc_vecs_db = SqliteDict(doc_vecs_db_path)
+    for query, _ in tqdm(queries2count, total=NUM_QUERIES, desc='Our System Step'):
+        doc_ids: list[str] = get_nearest(string2vec(query), return_all=True)
+        query2url_results[query] = [doc_vecs_db[doc_id].link for doc_id in doc_ids]
 
 
 if __name__ == '__main__':
