da7f1fa88aeeaac888005edc3eac17d479991374
Chris Wilhelm <70656080+ChrisWilhelm@users.noreply.github.com>
2021-12-10 15:04:03 -0500

    updated connection details and fixed bias id determination.

--- backend/load_db.py
+++ backend/load_db.py
@@ -2,14 +2,30 @@ import pickle
 from api import remove_repeat_articles
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+from backend.custom_types import Rating
 from db_types import *
 from random import random
 
 percent_articles_to_keep : float = 0.1
 
-engine = create_engine("mysql+pymysql://21fa_cwilhel8:Lg9CYYSiMP@dbase.cs.jhu.edu/21fa_cwilhel8_db")
+engine = create_engine("mysql+pymysql://db_final:password@54.211.230.209/db_final_db")
 Session = sessionmaker(bind = engine)
 session = Session()
+
+def determine_bias_id(article):
+    if article.news_source.rating == Rating.CENTER:
+        return 3
+    elif article.news_source.rating == Rating.LEFT:
+        return 1
+    elif article.news_source.rating == Rating.LEAN_LEFT:
+        return 2
+    elif article.news_source.rating == Rating.LEAN_RIGHT:
+        return 4
+    elif article.news_source.rating == Rating.RIGHT:
+        return 5
+    else:
+        return 6
 
 
 def news_source_exists(article):
@@ -24,7 +40,7 @@ def add_news_source(article):
         news_source = NewsSource(
             NewsSourceName=article.news_source.name,
             Homepage=article.news_source.url,
-            BiasID=article.news_source.rating
+            BiasID=determine_bias_id(article)
         )
         session.add(news_source)
         session.commit()
@@ -37,14 +53,14 @@ if __name__ == "__main__":
         add_news_source(article)
 
         news_source_id = session.query(NewsSource.NewsSourceID)\
-            .filter(name=article.news_source.name)\
+            .filter(NewsSource.NewsSourceName == article.news_source.name)\
             .first()
 
         new_article = Article(
             Aname=article.title,
             URL=article.url,
             PublishDate=article.publish_date,
-            NewsSourceID=news_source_id,
+            NewsSourceID=news_source_id[0],
             ArticleText=article.text,
             ArticleSummary=article.summary
         )
