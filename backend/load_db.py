70113d8abd1b05e67a082d315b7bd4aed1bbd287
Andrew King <agking10@gmail.com>
2021-12-11 15:33:59 -0500

    multithreaded upload

--- backend/load_db.py
+++ backend/load_db.py
@@ -1,20 +1,28 @@
 import pickle
+import argparse
+from typing import List
+
 from api import remove_repeat_articles
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 from backend.custom_types import Rating
 from db_types import *
-from tqdm import tqdm
 from random import random
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-percent_articles_to_keep : float = 0.3
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--percent_upload", type=float, default=0.1)
+args = parser.parse_args()
+
+percent_articles_to_keep : float = args.percent_upload
 aws_ip = "54.211.230.209"
 
 engine = create_engine("mysql+pymysql://db_final:password@" + aws_ip + "/db_final_db")
-# engine = create_engine("mysql+pymysql://21fa_cwilhel8:Lg9CYYSiMP@dbase.cs.jhu.edu/21fa_cwilhel8_db")
-Session = sessionmaker(bind = engine)
-session = Session()
+session_factory = sessionmaker(bind = engine)
+Session = scoped_session(session_factory)
+
 
 def determine_bias_id(article):
     if article.news_source.rating == Rating.CENTER:
@@ -31,86 +39,61 @@ def determine_bias_id(article):
         return 6
 
 
-def keyword_exists(word):
+def keyword_exists(word, session):
     return session.query(KeyWord.KeyWordID).filter(KeyWord.KeyWord == word).first() is not None
 
 
-def add_keywords(article):
-    for keyword in article.keywords:
-        if not keyword_exists(keyword):
-            key_word = KeyWord(
-                KeyWord=keyword
-            )
-            session.add(key_word)
-    session.commit()
+def author_exists(AName, session):
+    return session\
+               .query(Author.AuthorID)\
+               .filter(Author.AName == AName)\
+               .first() is not None
 
 
-def author_exists(AName):
-    return session.query(Author.AuthorID).filter(Author.AName == AName).first() is not None
-
-
-def add_authors(article):
-    for author in article.authors:
-        if not author_exists(author):
-            new_author = Author(
-                AName=author
-            )
-            session.add(new_author)
-    session.commit()
-
-
-def get_news_source(article):
+def get_news_source(article, session):
     return session \
      .query(NewsSource) \
      .filter_by(NewsSourceName=article.news_source.name) \
      .first()
 
 
-def add_news_source(article):
-    news_source: NewsSource = get_news_source(article)
+def add_news_source(article, session):
+    news_source: NewsSource = get_news_source(article, session)
     if news_source is None:
         news_source = NewsSource(
             NewsSourceName=article.news_source.name,
             Homepage=article.news_source.url,
-<<<<<<< Updated upstream
             BiasID=determine_bias_id(article)
-=======
-            BiasID=int(article.news_source.rating)
->>>>>>> Stashed changes
         )
         session.add(news_source)
         session.commit()
 
 
-<<<<<<< Updated upstream
-def article_not_included(article):
+def article_not_included(article, session):
     news_source_id = session.query(NewsSource.NewsSourceID).filter(NewsSource.NewsSourceName == article.news_source.name).first()
     if news_source_id is None :
         return True
-    return session.query(Article).filter(Article.Aname == article.title).filter(Article.NewsSourceID == news_source_id[0]).first() is None
-=======
-def get_author(author: Author):
+    return session.query(Article)\
+               .filter(Article.Aname == article.title)\
+               .filter(Article.NewsSourceID == news_source_id[0])\
+               .first() is None
+
+
+def get_author(author: Author, session):
     new_author = session.query(Author)\
-               .filter_by(FName=author.FName, LName=author.LName)\
+               .filter_by(AName=author.AName)\
                .first()
     return new_author
 
 
-def generate_author_obj(author : str) -> Author:
-    author_name = author.split()
-    first_name = author_name[0]
-    if len(author_name) > 1:
-        last_name = author_name[-1]
-    else:
-        last_name = ""
+def generate_author_obj(author : str, session) -> Author:
     new_author = Author(
-        FName=first_name,
-        LName=last_name
+        AName = author
     )
     return new_author
 
 
-def add_article(article) -> Article:
+def add_article(article, session) -> Article:
     news_source_id = session.query(NewsSource.NewsSourceID) \
         .filter_by(NewsSourceName=article.news_source.name) \
         .first()
@@ -129,9 +112,9 @@ def add_article(article) -> Article:
     return new_article
 
 
-def add_author(author: str) -> Author:
-    new_author: Author = generate_author_obj(author)
-    existing_author: Author = get_author(new_author)
+def add_author(author: str, session) -> Author:
+    new_author: Author = generate_author_obj(author, session)
+    existing_author: Author = get_author(new_author, session)
 
     if existing_author is None:
         session.add(new_author)
@@ -141,7 +124,7 @@ def add_author(author: str) -> Author:
     return existing_author
 
 
-def add_wrote_by(article_id: int, author_id: int) -> WroteBy:
+def add_wrote_by(article_id: int, author_id: int, session) -> WroteBy:
     new_wrote_by = WroteBy(
         ArticleID=article_id,
         AuthorID=author_id
@@ -149,75 +132,79 @@ def add_wrote_by(article_id: int, author_id: int) -> WroteBy:
     session.add(new_wrote_by)
     session.commit()
     return new_wrote_by
->>>>>>> Stashed changes
+
+
+def add_authors(article_id: int, authors: List[str], session):
+    for author in authors:
+        new_author: Author = add_author(author, session)
+
+        add_wrote_by(article_id, new_author.AuthorID, session)
+
+
+def find_keyword(word: str, session) -> KeyWord:
+    return session.query(KeyWord).filter_by(KeyWord=word).first()
+
+
+def add_keyword(word: str, session):
+    key_word = KeyWord(
+        KeyWord=word
+    )
+    session.add(key_word)
+    session.commit()
+    return key_word
+
+
+def add_has_keyword(article_id: int, keyword_id: int, session):
+    has_key_word = HasKeyWord(
+        KeyWordID=keyword_id,
+        ArticleID=article_id
+    )
+    session.add(has_key_word)
+    session.commit()
+
+
+def add_keywords(article_id: int, keywords: List[str], session):
+    for word in keywords:
+        key_word = find_keyword(word, session)
+        if key_word is None:
+            key_word = add_keyword(word, session)
+
+        add_has_keyword(article_id, key_word.KeyWordID, session)
+
+
+def process_article(article):
+    if random() > percent_articles_to_keep:
+        return
+    session = Session()
+    add_news_source(article, session)
+    new_article: Article = add_article(article, session)
+    add_authors(new_article.ArticleID, article.authors, session)
+    add_keywords(new_article.ArticleID, article.keywords, session)
+    Session.remove()
 
 
 if __name__ == "__main__":
     with open("../stories.pickle", "rb") as fp:
         articles = pickle.load(fp)
     articles = remove_repeat_articles(articles)
-<<<<<<< Updated upstream
-    # session.query(Article).delete()
-    # session.query(NewsSource).delete()
-    for article in articles:
-        if article_not_included(article):
-            add_news_source(article)
-            add_keywords(article)
-            add_authors(article)
-            news_source_id = session.query(NewsSource.NewsSourceID)\
-                .filter(NewsSource.NewsSourceName == article.news_source.name)\
-                .first()
-=======
+    session = Session()
     session.query(HasKeyWord).delete()
     session.query(WroteBy).delete()
     session.query(Article).delete()
     session.query(NewsSource).delete()
     session.query(Author).delete()
-    for article in tqdm(articles):
-        if random() > percent_articles_to_keep:
-            continue
-        add_news_source(article)
+    session.commit()
+    Session.remove()
 
-        new_article: Article = add_article(article)
+    print("Processing %d articles" % len(articles))
+    articles_processed = 0
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        futures = [executor.submit(process_article, article) for article in articles]
+        for i, future in enumerate(as_completed(futures)):
+            articles_processed += 1
+            if articles_processed % 500 == 0:
+                print("{n} articles processed".format(n=articles_processed))
 
-        for author in article.authors:
-            new_author: Author = add_author(author)
+    print("Done!")
+    Session.remove()
 
-            add_wrote_by(new_article.ArticleID, new_author.AuthorID)
-        break
->>>>>>> Stashed changes
-
-            new_article = Article(
-                Aname=article.title,
-                URL=article.url,
-                PublishDate=article.publish_date,
-                NewsSourceID=news_source_id[0],
-                ArticleText=article.text,
-                ArticleSummary=article.summary
-            )
-            session.add(new_article)
-            session.commit()
-            article_id = session.query(Article.ArticleID).filter(Article.Aname == new_article.Aname)\
-                .filter(new_article.NewsSourceID == Article.NewsSourceID).first()
-            for keyword in article.keywords:
-                key_wordid = session.query(KeyWord.KeyWordID).filter(KeyWord.KeyWord == keyword).first()
-                has_keyword = HasKeyWord(
-                    KeyWordID=key_wordid[0],
-                    ArticleID=article_id[0]
-                )
-                session.add(has_keyword)
-            session.commit()
-            for author in article.authors:
-                author_id = session.query(Author.AuthorID).filter(Author.AName == author).first()
-                new_written_by = WroteBy(
-                    ArticleID=article_id[0],
-                    AuthorID=author_id[0]
-                )
-                session.add(new_written_by)
-            session.commit()
-            # for author in article.authors:
-            #     add_author()
-            break
-    result = session.query(Article).all()
-    for row in result:
-        print("Title: ", row.Aname)
